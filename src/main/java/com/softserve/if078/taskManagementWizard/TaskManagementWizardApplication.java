95195018aa1c9e29fcf52b6b01628fcff5ad8b1e
Dimitrij512 <hfl-if@ukr.net>
2017-09-15 12:05:32 +0300

    first commit

---
+++ src/main/java/com/softserve/if078/taskManagementWizard/TaskManagementWizardApplication.java
@@ -0,0 +1,12 @@
+package com.softserve.if078.taskManagementWizard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementWizardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagementWizardApplication.class, args);
+	}
+}
